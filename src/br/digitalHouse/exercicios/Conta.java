c8af220d8a7bb97f75f5b71822269aaa1002e2fb
Nina Lofrese <nina.lofrese@gmail.com>
2019-08-07 16:04:36 -0300

    Resolução em casa do exercício em aula

--- src/br/digitalHouse/exercicios/Conta.java
+++ src/br/digitalHouse/exercicios/Conta.java
@@ -2,20 +2,41 @@ package br.digitalHouse.exercicios;
 
 public abstract class Conta {
     private double saldo;
+    private Cliente cliente;
 
-    public Conta(double novoSaldo){
-        saldo = novoSaldo;
+    public void depositar(double quantia) {
+        System.out.println("Realizando depósito... quantia R$ " + quantia);
+        this.saldo += quantia;
+        System.out.println("Depósito realizado" + this.cliente.getNome() + ".\nSaldo atualizado: " + this.saldo);
     }
 
-    public abstract String fazerDeposito(double valor);
-    public abstract boolean sacarDinheiro(double valor);
-    public abstract void consultarSaldo();
+    public void sacar(double quantia) {
+        System.out.println("Realizando saque... quantia R$ " + quantia);
 
+        if (saldo < quantia) {
+            System.out.println("Saldo insuficiente.\nVocê tá sem grana, " + this.cliente.getNome());
+            return;
+        }
+
+        this.saldo -= quantia;
+        System.out.println("Saque realizado " + this.cliente.getNome() + "\nSaldo atualizado: " + this.saldo);
+
+    }
+
+    // consultar saldo
     public double getSaldo() {
         return saldo;
     }
 
-    public void setSaldo(double saldo){
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
