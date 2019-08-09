7892b92daec71923fc0c4f617b9582ba88f25dd4
Nina Lofrese <nina.lofrese@gmail.com>
2019-08-08 22:18:27 -0300

    Aula de interface

---
+++ src/br/digitalHouse/bancoSantander/Principal.java
@@ -0,0 +1,7 @@
+package br.digitalHouse.bancoSantander;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
--- src/br/digitalHouse/exercicios/ContaPoupanca.java
+++
@@ -1,9 +0,0 @@
-package br.digitalHouse.exercicios;
-
-public class ContaPoupanca extends Conta {
-    private double taxa;
-
-    public void recolherJuros() {
-        this.setSaldo(this.getSaldo() + (taxa / 100));
-    }
-}
